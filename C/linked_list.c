4b9aa0b8d0af63c905d73f11524937e2fd4fd1a6
nathan3o4 <nathan3o4@gmail.com>
2016-12-17 16:27:51 -0600

    Adds a Linked List in C
    Closes #1

---
+++ C/linked_list.c
@@ -0,0 +1,167 @@
+#import <stdio.h>
+#import <string.h>
+
+// Define Node
+typedef struct Node_s {
+  int value;
+  struct Node_s* next;
+} Node;
+const Node NODE_INIT = {
+  0, NULL
+};
+
+// Define Linked List
+typedef struct LinkedList_s {
+  int length;
+  struct Node_s* head;
+} LinkedList;
+const LinkedList LINKEDLIST_INIT = {
+  0, NULL
+};
+
+// Methods
+Node* getNode(LinkedList* list, int index) {
+  if (list == NULL) {
+    printf("ERROR: LinkedList* list is NULL.\n");
+    return NULL;
+  }
+  if (index < 0) {
+    printf("ERROR: Index value cannot be negative.\n");
+    return NULL;
+  }
+  if (index >= list->length) {
+    printf("ERROR: Index value is out of range.\n");
+    return NULL;
+  }
+  Node* currentNode = list->head;
+  for (int i = 0; i < index; i++) {
+    currentNode = currentNode->next;
+  }
+  return currentNode;
+}
+
+void append(LinkedList* list, Node* newNode) {
+  if (list == NULL) {
+    printf("ERROR: LinkedList* list is NULL.\n");
+    return;
+  }
+  if (newNode == NULL) {
+    printf("ERROR: Node* newNode is NULL.\n");
+    return;
+  }
+  if (list->length == 0) {
+    list->head = newNode;
+    list->length++;
+    return;
+  }
+  Node* currentNode = list->head;
+  while (currentNode->next != NULL) {
+    currentNode = currentNode->next;
+  }
+  currentNode->next = newNode;
+  list->length++;
+  return;
+}
+
+void prepend(LinkedList* list, Node* newNode) {
+  if (list == NULL) {
+    printf("ERROR: LinkedList* list is NULL.\n");
+    return;
+  }
+  if (newNode == NULL) {
+    printf("ERROR: Node* node is NULL.\n");
+    return;
+  }
+  if (list->length == 0) {
+    list->head = newNode;
+    list->length++;
+    return;
+  }
+  Node* currentNode = list->head;
+  list->head = newNode;
+  newNode->next = currentNode;
+  list->length++;
+  return;
+}
+
+void insert(LinkedList* list, Node* newNode, int index) {
+  if (list == NULL) {
+    printf("ERROR: LinkedList* list is NULL.\n");
+    return;
+  }
+  if (newNode == NULL) {
+    printf("ERROR: Node* newNode is NULL.\n");
+    return;
+  }
+  if (index < 0) {
+    printf("ERROR: Index cannot be negative.\n");
+    return;
+  }
+  if (index > list->length) {
+    printf("ERROR: Index is out of bounds.\n");
+    return;
+  }
+  if (index == 0) {
+    prepend(list, newNode);
+  }
+  if (index == list->length) {
+    append(list, newNode);
+  }
+  Node* previousNode = getNode(list, index - 1);
+  Node* currentNode = getNode(list, index);
+  previousNode->next = newNode;
+  newNode->next = currentNode;
+  list->length++;
+  return;
+}
+
+void delete(LinkedList* list, int index) {
+  if (list == NULL) {
+    printf("ERROR: LinkedList* list is NULL.\n");
+    return;
+  }
+  if (list->length == 0) {
+    printf("ERROR: Cannot delete from an empty list.\n");
+    return;
+  }
+  if (index < 0) {
+    printf("ERROR: Index cannot be negative.\n");
+    return;
+  }
+  if (index >= list->length) {
+    printf("ERROR: Index is out of bounds.\n");
+    return;
+  }
+  Node* previousNode = getNode(list, index - 1);
+  Node* currentNode = getNode(list, index);
+  previousNode->next = currentNode->next;
+  currentNode->next = NULL;
+  list->length--;
+}
+
+void printList(LinkedList* list) {
+  if (list == NULL) {
+    printf("ERROR: LinkedList* list is NULL.\n");
+    return;
+  }
+  printf("[\n");
+  for (int i = 0; i < list->length; i++) {
+    printf("  %d\n", getNode(list, i)->value);
+  }
+  printf("]\n");
+}
+
+int main(int argc, char const *argv[]) {
+  LinkedList list = LINKEDLIST_INIT;
+
+  Node node1 = NODE_INIT;
+  node1.value = 1;
+  append(&list, &node1);
+
+  Node node2 = NODE_INIT;
+  node2.value = 2;
+  prepend(&list, &node2);
+
+  printList(&list);
+  return 0;
+}
