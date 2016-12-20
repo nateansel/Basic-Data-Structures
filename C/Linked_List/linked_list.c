ae8ea204033d7f3c0e890d50fc08e3dc4f8bce0e
nathan3o4 <nathan3o4@gmail.com>
2016-12-19 22:27:40 -0600

    Fixes a bug with LinkedList deletion in C

--- C/Linked_List/linked_list.c
+++ C/Linked_List/linked_list.c
@@ -1,36 +1,41 @@
-#import <stdio.h>
-#import <string.h>
+#ifndef linked_list_c
+#define linked_list_c
+
+#ifndef stdio_h
+#define stdio_h
+#include <stdio.h>
+#endif
 
 // Define Node
 typedef struct Node_s {
   int value;
   struct Node_s* next;
 } Node;
-const Node NODE_INIT = {
-  0, NULL
-};
+#define NODE_INIT(param, value) \
+{ .param = value, .next = NULL }
 
 // Define Linked List
 typedef struct LinkedList_s {
   int length;
   struct Node_s* head;
 } LinkedList;
-const LinkedList LINKEDLIST_INIT = {
-  0, NULL
-};
+#define LINKEDLIST_INIT() { .length = 0, .head = NULL }
 
 // Methods
 Node* getNode(LinkedList* list, int index) {
   if (list == NULL) {
     printf("ERROR: LinkedList* list is NULL.\n");
+    fflush(stdout);
     return NULL;
   }
   if (index < 0) {
     printf("ERROR: Index value cannot be negative.\n");
+    fflush(stdout);
     return NULL;
   }
   if (index >= list->length) {
     printf("ERROR: Index value is out of range.\n");
+    fflush(stdout);
     return NULL;
   }
   Node* currentNode = list->head;
@@ -43,10 +48,12 @@ Node* getNode(LinkedList* list, int index) {
 void append(LinkedList* list, Node* newNode) {
   if (list == NULL) {
     printf("ERROR: LinkedList* list is NULL.\n");
+    fflush(stdout);
     return;
   }
   if (newNode == NULL) {
     printf("ERROR: Node* newNode is NULL.\n");
+    fflush(stdout);
     return;
   }
   if (list->length == 0) {
@@ -66,10 +73,12 @@ void append(LinkedList* list, Node* newNode) {
 void prepend(LinkedList* list, Node* newNode) {
   if (list == NULL) {
     printf("ERROR: LinkedList* list is NULL.\n");
+    fflush(stdout);
     return;
   }
   if (newNode == NULL) {
     printf("ERROR: Node* node is NULL.\n");
+    fflush(stdout);
     return;
   }
   if (list->length == 0) {
@@ -87,18 +96,22 @@ void prepend(LinkedList* list, Node* newNode) {
 void insert(LinkedList* list, Node* newNode, int index) {
   if (list == NULL) {
     printf("ERROR: LinkedList* list is NULL.\n");
+    fflush(stdout);
     return;
   }
   if (newNode == NULL) {
     printf("ERROR: Node* newNode is NULL.\n");
+    fflush(stdout);
     return;
   }
   if (index < 0) {
     printf("ERROR: Index cannot be negative.\n");
+    fflush(stdout);
     return;
   }
   if (index > list->length) {
     printf("ERROR: Index is out of bounds.\n");
+    fflush(stdout);
     return;
   }
   if (index == 0) {
@@ -118,18 +131,29 @@ void insert(LinkedList* list, Node* newNode, int index) {
 void delete(LinkedList* list, int index) {
   if (list == NULL) {
     printf("ERROR: LinkedList* list is NULL.\n");
+    fflush(stdout);
     return;
   }
   if (list->length == 0) {
     printf("ERROR: Cannot delete from an empty list.\n");
+    fflush(stdout);
     return;
   }
   if (index < 0) {
     printf("ERROR: Index cannot be negative.\n");
+    fflush(stdout);
     return;
   }
   if (index >= list->length) {
     printf("ERROR: Index is out of bounds.\n");
+    fflush(stdout);
+    return;
+  }
+  if (index == 0) {
+    Node* currentNode = getNode(list, index);
+    list->head = currentNode->next;
+    currentNode->next = NULL;
+    list->length--;
     return;
   }
   Node* previousNode = getNode(list, index - 1);
@@ -137,6 +161,7 @@ void delete(LinkedList* list, int index) {
   previousNode->next = currentNode->next;
   currentNode->next = NULL;
   list->length--;
+  return;
 }
 
 void printList(LinkedList* list) {
@@ -144,24 +169,34 @@ void printList(LinkedList* list) {
     printf("ERROR: LinkedList* list is NULL.\n");
     return;
   }
-  printf("[\n");
+  printf("List:     %d\nElements: %d\n[\n", list, list->length);
   for (int i = 0; i < list->length; i++) {
-    printf("  %d\n", getNode(list, i)->value);
+    if (i == (list->length - 1)) {
+      printf("  %d\n", getNode(list, i)->value);
+    } else {
+      printf("  %d,\n", getNode(list, i)->value);
+    }
   }
   printf("]\n");
 }
 
-int main(int argc, char const *argv[]) {
-  LinkedList list = LINKEDLIST_INIT;
+// #ifndef main_c
+//
+// int main(int argc, char const *argv[]) {
+//   LinkedList list = LINKEDLIST_INIT();
+//
+//   Node node1 = NODE_INIT(value, 1);
+//   // node1.value = 1;
+//   append(&list, &node1);
+//
+//   Node node2 = NODE_INIT(value, 2);
+//   // node2.value = 2;
+//   prepend(&list, &node2);
+//
+//   printList(&list);
+//   return 0;
+// }
+//
+// #endif
 
-  Node node1 = NODE_INIT;
-  node1.value = 1;
-  append(&list, &node1);
-
-  Node node2 = NODE_INIT;
-  node2.value = 2;
-  prepend(&list, &node2);
-
-  printList(&list);
-  return 0;
-}
+#endif
